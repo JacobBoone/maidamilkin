4a3eced20fbe5ceaf8f572eee006b3fa09c77720
Jacob <jakeboone@aol.com>
2014-08-05 16:38:17 -0600

    shelled out some of the general look and layout

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var indexController = require('./controllers/index.js');
 
+
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/milkinblog')
 
